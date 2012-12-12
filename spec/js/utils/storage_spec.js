55b8276b0e7caae14c8ef6ae1a014ec3d9e09d44
Pavel Motorin <pavel.motorin@inn.ru>
2012-12-12 21:37:36 +0400

    test on storage

--- spec/js/utils/storage_spec.js
+++ spec/js/utils/storage_spec.js
@@ -1,7 +1,25 @@
 (function() {
+  var localStorage, mockups;
 
-  describe("Storage module", function() {
-    return describe("Check interface", function() {
+  mockups = localStorage = {
+    store: {},
+    setItem: function(moduleName, key, value) {
+      this.store["" + moduleName + "/" + key] = value;
+      return true;
+    },
+    getItem: function(moduleName, key) {
+      return this.store["" + moduleName + "/" + key];
+    },
+    removeItem: function(moduleName, key) {
+      return this.store["" + moduleName + "/" + key] = null;
+    },
+    getKeys: function() {
+      return this.store;
+    }
+  };
+
+  describe("[Storage module]", function() {
+    describe("Module interface", function() {
       var storage;
       storage = null;
       beforeEach(function() {
@@ -19,6 +37,68 @@
           expect(storage.get).toBeFunction();
           expect(storage.remove).toBeFunction();
           return expect(storage.getKeys).toBeFunction();
+        });
+      });
+    });
+    return describe("Saving key/value", function() {
+      var storage, _oldCookie, _oldLocalStorage, _oldSessionStorage;
+      storage = null;
+      _oldLocalStorage = null;
+      _oldSessionStorage = null;
+      _oldCookie = null;
+      beforeEach(function() {
+        storage = null;
+        _oldLocalStorage = window.localStorage;
+        _oldSessionStorage = window.sessionStorage;
+        return require(["utils/storage"], function(storageModule) {
+          return storage = storageModule;
+        });
+      });
+      afterEach(function() {
+        window.localStorage = _oldLocalStorage;
+        return window.sessionStorage = _oldSessionStorage;
+      });
+      it("should save given key/value pair (both must be strings) with 'moduleName'-prefix to localStorage only (4-th parameter is false). If possible. ", function() {
+        waitsFor(function() {
+          return storage !== null;
+        });
+        return runs(function() {
+          if (typeof window.localStorage === "undefined") {
+            window.localStorage = mockups.localStorage;
+          }
+          storage.save("testModule", "testKey", "testValue");
+          expect(window.localStorage.getItem("testModule/testKey")).toBe("testValue");
+          return expect(window.sessionStorage.getItem("testModule/testKey")).toBe(null);
+        });
+      });
+      it("should save given key/value pair (both must be strings) with 'moduleName'-prefix to sessionStorage only (4-th parameter is true). If possible.", function() {
+        waitsFor(function() {
+          return storage !== null;
+        });
+        return runs(function() {
+          if (typeof window.localStorage === "undefined") {
+            window.localStorage = mockups.localStorage;
+          }
+          if (typeof window.localStorage === "undefined") {
+            window.localStorage = mockups.localStorage;
+          }
+          storage.save("testModule", "testKey", "testValue", true);
+          expect(window.sessionStorage.getItem("testModule/testKey")).toBe("testValue");
+          return expect(window.localStorage.getItem("testModule/testKey")).toBe(null);
+        });
+      });
+      return it("should'nt save to anywahere given key/value pair with 'moduleName'-prefix if 'value' isn't string", function() {
+        waitsFor(function() {
+          return storage !== null;
+        });
+        return runs(function() {
+          _oldLocalStorage = window.localStorage;
+          if (typeof window.localStorage === "undefined") {
+            window.localStorage = mockups.localStorage;
+          }
+          storage.save("testModule", "testKey", "testValue");
+          expect(window.localStorage.getItem("testModule/testKey")).toBe("testValue");
+          return window.localStorage = _oldLocalStorage;
         });
       });
     });
